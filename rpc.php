7c153da1b4474c7dddaa698dc681d55e68c53f04
MSFTserver <sawyerlemmo@gmail.com>
2018-08-01 16:37:17 -0700

    update licenses

--- rpc.php
+++ rpc.php
@@ -7,6 +7,8 @@ A simple class for making calls to Bitcoin's API using PHP.
 https://github.com/aceat64/EasyBitcoin-PHP
 ====================
 The MIT License (MIT)
+Copyright (c) 2018 MSFTserver
+Copyright (c) 2018 RealBokito
 Copyright (c) 2013 Andrew LeCody
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
